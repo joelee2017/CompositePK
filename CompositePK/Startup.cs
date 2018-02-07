d43e3e9356729d219efd41e31a4daf1f578e7eb1
joelee2017 <as955133@gmail.com>
2018-02-07 10:30:50 +0800

    加入專案檔案。

---
+++ CompositePK/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CompositePK.Startup))]
+namespace CompositePK
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
