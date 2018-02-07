2923ca3f199b777dfb71786ffbf7d52eea1c3975
joelee2017 <as955133@gmail.com>
2018-02-07 10:47:25 +0800

    ASP.NET MVC網站控制器與多主鍵資料表操作

---
+++ CompositePK/Controllers/Order_DetailsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CompositePK.Models;
+
+namespace CompositePK.Controllers
+{
+    public class Order_DetailsController : Controller
+    {
+        private NorthwindEntities db = new NorthwindEntities();
+
+        // GET: Order_Details
+        public ActionResult Index()
+        {
+            return View(db.Order_Details.ToList());
+        }
+
+        // GET: Order_Details/Details/5
+        public ActionResult Details(int? id,int? ProductID)
+        {
+            if (id == null || ProductID ==null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Order_Details order_Details = db.Order_Details.Find(new object[] {id,ProductID });
+            if (order_Details == null)
+            {
+                return HttpNotFound();
+            }
+            return View(order_Details);
+        }
+
+        // GET: Order_Details/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Order_Details/Create
+        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+        // 詳細資訊，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "OrderID,ProductID,UnitPrice,Quantity,Discount")] Order_Details order_Details)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Order_Details.Add(order_Details);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(order_Details);
+        }
+
+        // GET: Order_Details/Edit/5
+        public ActionResult Edit(int? id, int? ProductID)
+        {
+            if (id == null || ProductID == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Order_Details order_Details = db.Order_Details.Find(new object[] { id, ProductID });
+            if (order_Details == null)
+            {
+                return HttpNotFound();
+            }
+            return View(order_Details);
+        }
+
+        // POST: Order_Details/Edit/5
+        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+        // 詳細資訊，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "OrderID,ProductID,UnitPrice,Quantity,Discount")] Order_Details order_Details)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(order_Details).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(order_Details);
+        }
+
+        // GET: Order_Details/Delete/5
+        public ActionResult Delete(int? id, int? ProductID)
+        {
+            if (id == null || ProductID == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Order_Details order_Details = db.Order_Details.Find(new object[] { id, ProductID });
+            if (order_Details == null)
+            {
+                return HttpNotFound();
+            }
+            return View(order_Details);
+        }
+
+        // POST: Order_Details/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id,int ProductID)//前面檢查過所以就不用在加 ? 問號代表允許null值
+        {
+            Order_Details order_Details = db.Order_Details.Find(new object[] { id, ProductID });
+            db.Order_Details.Remove(order_Details);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
